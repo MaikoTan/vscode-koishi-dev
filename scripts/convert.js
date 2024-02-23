b244b6fac4764111a087e89e6cbd99de246a767c
Maiko Tan <maiko.tan.coding@gmail.com>
2024-02-23 17:24:16 +0800

    refactor: migrate convert script to single script file

---
+++ scripts/convert.js
@@ -0,0 +1,16 @@
+const fs = require('fs')
+const yaml = require('js-yaml')
+
+const files = [
+  'schemata/package-json.yaml',
+  'schemata/koishi-yml.yaml',
+  'snippets/koishi.common.yaml',
+]
+
+;(async () => {
+  for (const file of files) {
+    const content = await fs.promises.readFile(file, 'utf-8')
+    const data = yaml.load(content)
+    await fs.promises.writeFile(file.replace(/\.yaml$/, '.json'), JSON.stringify(data, null, 2))
+  }
+})()
